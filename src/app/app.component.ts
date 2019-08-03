57a679379e5c4dd92f906578abfa5b238ce8c5b2
oana <oanapatru@gmail.com>
2019-08-03 14:22:02 -0400

    first push

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import {isPlatformBrowser} from '@angular/common';
+
+import { AuthService } from './auth/auth.service';
+import { LoggingService } from './logging.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  constructor(
+    private authService: AuthService,
+    private loggingService: LoggingService,
+    @Inject(PLATFORM_ID) private platformId
+  ) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.authService.autoLogin();
+    }
+    this.loggingService.printLog('Hello from AppComponent ngOnInit');
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  baseUrl: 'https://ng-complete-guide-e4fc7.firebaseio.com/',
+  apiKey: 'AIzaSyBaGC5C5Ii8y5-6doTUcStwiEiRGTawdR8',
+  oldsignInUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken',
+  oldloginUpUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp',
+  signupUrl: 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser',
+  loginUrl: 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword'
+};
