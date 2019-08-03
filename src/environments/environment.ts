6abce30e2ed5f3657d2be78ce933fa8974493b5b
oana <oanapatru@gmail.com>
2019-08-03 14:32:57 -0400

    commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,12 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://ng-complete-guide-e4fc7.firebaseio.com/',
-  apiKey: 'AIzaSyBaGC5C5Ii8y5-6doTUcStwiEiRGTawdR8',
-  oldsignInUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken',
-  oldloginUpUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp',
-  signupUrl: 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser',
-  loginUrl: 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword'
+  baseUrl: '',
+  apiKey: '',
+  oldsignInUrl: '',
+  oldloginUpUrl: '',
+  signupUrl: '',
+  loginUrl: ''
 
 };
 
